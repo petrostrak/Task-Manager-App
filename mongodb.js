d49afc043f34dd168cad648832ab68d4849ed374
petros <pit.trak@gmail.com>
2020-08-02 20:05:02 +0300

    Connecting and Inserting Documents

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+// CRUD
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const database = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        return console.log('Unable to connect to database!');
+    }
+    
+    const db = client.db(database)
+    db.collection('users').insertOne({
+        name: 'Petros',
+        age: 34
+    })
+})
