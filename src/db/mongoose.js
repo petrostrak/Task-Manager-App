936bc5d8c7bb07320bea9045efff1a2f4b31c0e0
petros <pit.trak@gmail.com>
2020-08-03 12:58:49 +0300

    Resource creation endpoints (post /users)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+// https://mongoosejs.com/docs/validation.html#built-in-validators
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -19,52 +20,15 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const task = new Task({
-    description: 'A morning exercise'
-}).save().then(() => {
-    console.log(task);
-}).catch((e) => {
-    console.log('Error!', e)
-})
+// const task = new Task({
+//     description: 'A morning exercise'
+// }).save().then(() => {
+//     console.log(task);
+// }).catch((e) => {
+//     console.log('Error!', e)
+// })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    }, 
-    email: {
-        type: String, 
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0, 
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }, 
-    password: {
-        type: String,
-        required: true,
-        minlength: 7, 
-        trim: true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password should not contain the word "password"')
-            }
-        }
-    }
-})
+
 
 // const me = new User({
 //     name: 'Maria',
