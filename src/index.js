f40edd9ffdac78a4273fee37cb92367e437c2e4b
petros <pit.trak@gmail.com>
2020-08-04 19:13:15 +0300

    Express Middleware (maintenance situation)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ require('./db/mongoose')
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const { response } = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// app.use((req, res, next) => {
+//     if(req.method === 'GET'){
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('The website is under maintenance, please try again later')
+})
 
 app.use(express.json())
 app.use(userRouter)
