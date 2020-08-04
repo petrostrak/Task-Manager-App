4bd1fe4dc8e40519d5127c5eb903fe5c86677aa3
petros <pit.trak@gmail.com>
2020-08-04 11:12:37 +0300

    Integrating async - await

--- src/index.js
+++ src/index.js
@@ -11,72 +11,78 @@ app.use(express.json())
 // GET for resource reading
 // https://mongoosejs.com/docs/queries.html
 // GET ALL USERS
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    const users = await User.find({})
+    try {
         res.status(200).send(users)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e)
-    })
+    }
 })
 
 // GET ONE USER
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
-        if(!user){
+
+    try {
+        const user = await User.findById(_id)
+
+        if (!user) {
             return res.status(404).send()
         }
 
         res.status(200).send(user)
-
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e)
-    })
+    }
 })
 
 // GET ALL TASKS
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find({})
+
+    try{
         res.status(200).send(tasks)
-    }).catch((e) => {
+    }catch(e){
         res.status(500).send(e)
-    })
+    }
 })
 
 // GET ONE TASK
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
-        if(!task){
+    const task = await Task.findById(_id)
+    try{
+        if (!task) {
             return res.status(404).send()
         }
-
         res.status(200).send(task)
-
-    }).catch((e) => {
+    }catch(e){
         res.status(500).send(e)
-    })
+    }
 })
 
 // POST for resource creation
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e);
-    })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try{
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
+    }catch(e){
         res.status(400).send(e)
-    })
+    }
 })
 
 app.listen(port, () => {
