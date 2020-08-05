c30f215fd4702ee4747c8763ed7a6bc383dba665
petros <pit.trak@gmail.com>
2020-08-05 18:28:09 +0300

    Auto-Cropping and image formatting

--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
 const express = require('express')
 require('./db/mongoose')
-
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const { response } = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000 // a million bytes = 1 mb
-    },
-    fileFilter(req, file, cb){ //cb = callback
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('File upload a Word document'))
-        }
-
-        cb(undefined, true)
-
-        // cb(new Error('File must be a PDF'))
-        // cb(undefined, true)
-        // cb(undefined, false)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
-
-
+  
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
