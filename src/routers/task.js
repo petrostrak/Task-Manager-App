b087e80ed1f657e634746289d321e74b8125e9fd
petros <pit.trak@gmail.com>
2020-08-05 13:17:01 +0300

    Added pagination to task route

--- src/routers/task.js
+++ src/routers/task.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth')
 
 // GET ALL TASKS
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
 
@@ -16,7 +17,11 @@ router.get('/tasks', auth, async (req, res) => {
         // const tasks = await Task.find({ author: req.user._id })
         await req.user.populate({ 
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.status(200).send(req.user.tasks)
     } catch (e) {
