239eb36708227f482c8a6301e8efda70d2a527f3
petros <pit.trak@gmail.com>
2020-08-05 18:15:34 +0300

    Serving up files

--- src/routers/user.js
+++ src/routers/user.js
@@ -125,10 +125,26 @@ router.post('/users/me/avatar', auth, avatar.single('avatar'), async (req, res) 
     res.status(400).send({error: error.message})
 })
 
+// DELETE profile pic
 router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    }catch(e){
+        res.status(404).send()
+    }
 })
 
 module.exports = router
